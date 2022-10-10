28058cec85281f5bc6ddb1466f10a023eb04932e
neha2003 <neha.sq8@gmail.com>
2022-10-10 16:19:57 -0500

    adding buttons to cashier gui

--- Cashier.java
+++ Cashier.java
@@ -1,16 +1,43 @@
+import java.awt.*;
 import javax.swing.*;
-public class Cashier {
+import java.awt.event.*;
+public class Cashier extends JFrame {
     
     JFrame frame = new JFrame();
-    JLabel label = new JLabel("This is the cashier window");
+    static JToolBar tb;
+    static JButton breakfast, entree, salads, sides, kids, treats, drinks, dipping;
 
     Cashier() {
-        label.setBounds(0, 0, 100, 50);
-        frame.add(label);
+        // frame.add(label);
+        frame.setLayout(new BorderLayout());
+        tb = new JToolBar();
+ 
+        // create a panel
+        JPanel p = new JPanel();
 
+        breakfast = new JButton("Breakfast");
+        entree = new JButton("Entree");
+        salads = new JButton("Salads");
+        sides = new JButton("Sides");
+        kids = new JButton("Kids Meals");
+        treats = new JButton("Treats");
+        drinks = new JButton("Drinks");
+        dipping = new JButton("Dipping sauces & Dressings");
+
+        // add buttons
+        p.add(breakfast);
+        p.add(entree);
+        p.add(salads);
+        p.add(sides);
+        p.add(kids);
+        p.add(treats);
+        p.add(drinks);
+        p.add(dipping);
+
+        frame.add(tb, BorderLayout.NORTH);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Cashier GUI");
-        frame.pack();
+        frame.setSize(500, 500);
         frame.setVisible(true);
 
     }
