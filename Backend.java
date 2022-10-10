1f53d9806e44b0aa188881c72ac56a51f47757b3
unknown <krishnan.prashanth@tamu.edu>
2022-10-10 04:16:28 -0500

    Add connectionChecks to the database for each backend function

--- Backend.java
+++ Backend.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 public class Backend {
     
     static Connection conn; // This is the connection that will be utilized by the rest of the Java Connections. 
-    static Statement stmt; 
+    static Statement stmt;
 
     public static void createConnection() 
     {
@@ -43,33 +43,50 @@ public class Backend {
 
     boolean isValue(String tableName, String fieldName, String value)
     {
+        // If a connection to the query does not already exist, we need to create that connection. 
+        if(conn == null || stmt == null) createConnection();
         
         return true;
     }
 
     String[] getValue(String tableName, String fieldName, String value)
     {
+        // If a connection to the query does not already exist, we need to create that connection. 
+        if(conn == null || stmt == null) createConnection();
+
         return null;
     }
 
     String[][] getNValues(String tableName, String fieldName, String value)
     {
+        // If a connection to the query does not already exist, we need to create that connection. 
+        if(conn == null || stmt == null) createConnection();
+
         return null;
     }
 
     boolean addValue(String tableName, String[] record)
     {
+        // If a connection to the query does not already exist, we need to create that connection. 
+        if(conn == null || stmt == null) createConnection();
+
         return true;
     }
 
 
     boolean[] addNValues(String tableName, String[][] records)
     {
+        // If a connection to the query does not already exist, we need to create that connection. 
+        if(conn == null || stmt == null) createConnection();
+
         return null;
     }
     
     ResultSet runQuery(String sqlQuery)
     {
+        // If a connection to the query does not already exist, we need to create that connection. 
+        if(conn == null || stmt == null) createConnection();
+        
         return null;
     }
     
