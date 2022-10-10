af22ebd45e9e4ed1b8a4fa44eff2250305a8e9f1
mochiflootloops <m.ghosh.3522@gmail.com>
2022-10-10 17:45:44 -0500

    Created manager launchpad, added windows for inventory and menu editing

---
+++ MenuEditor.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class MenuEditor implements ActionListener {
+
+    private JFrame frame;
+    private JPanel panel;
+
+    MenuEditor() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == inventory_button) {
+            frame.dispose();
+            new Inventory();
+        }
+        if (e.getSource() == menu_button) {
+            frame.dispose();
+            new MenuEditor();
+        }
+    }
+}
