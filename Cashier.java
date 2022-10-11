bb26faca3708d3dac87fd50636316dc134f12006
neha2003 <neha.sq8@gmail.com>
2022-10-10 21:42:47 -0500

    added rest of the menu categories to cashier gui

--- Cashier.java
+++ Cashier.java
@@ -31,24 +31,29 @@ public class Cashier implements ActionListener {
         tempSalads.add(new String[]{"Item#6", "2.89"});
         menuItems.put("Salads", tempSalads);
 
+        ArrayList<String[]> tempSides = new ArrayList<>();
+        tempSides.add(new String[]{"Item#7", "6.00"});
+        tempSides.add(new String[]{"Item#8", "2.00"});
+        menuItems.put("Sides", tempSides);
+
         ArrayList<String[]> kidsMeals = new ArrayList<>();
-        kidsMeals.add(new String[]{"Item#7", "2.58"});
-        kidsMeals.add(new String[]{"Item#8", "9.99"});
+        kidsMeals.add(new String[]{"Item#9", "2.58"});
+        kidsMeals.add(new String[]{"Item#10", "9.99"});
         menuItems.put("Kids_Meals", kidsMeals);
 
         ArrayList<String[]> treats = new ArrayList<>();
-        treats.add(new String[]{"Item#9", "3.71"});
-        treats.add(new String[]{"Item#10", "6.89"});
+        treats.add(new String[]{"Item#11", "3.71"});
+        treats.add(new String[]{"Item#12", "6.89"});
         menuItems.put("Treats", treats);
 
         ArrayList<String[]> drinks = new ArrayList<>();
-        drinks.add(new String[]{"Item#11", "3.71"});
-        drinks.add(new String[]{"Item#12", "6.89"});
+        drinks.add(new String[]{"Item#13", "3.71"});
+        drinks.add(new String[]{"Item#14", "6.89"});
         menuItems.put("Drinks", drinks);
 
         ArrayList<String[]> sauces = new ArrayList<>();
-        sauces.add(new String[]{"Item#13", "7.91"});
-        sauces.add(new String[]{"Item#14", "5.39"});
+        sauces.add(new String[]{"Item#15", "7.91"});
+        sauces.add(new String[]{"Item#16", "5.39"});
         menuItems.put("Sauces", sauces);
     }
 
@@ -83,6 +88,11 @@ public class Cashier implements ActionListener {
         breakfast.addActionListener(this);
         entree.addActionListener(this);
         salads.addActionListener(this);
+        sides.addActionListener(this);
+        kids.addActionListener(this);
+        treats.addActionListener(this);
+        drinks.addActionListener(this);
+        dipping.addActionListener(this);
         
         frame.add(menu_panel, BorderLayout.PAGE_START);
         frame.add(tb, BorderLayout.NORTH);
@@ -91,6 +101,11 @@ public class Cashier implements ActionListener {
         card_panel.add(breakfast_panel(), "breakfast");
         card_panel.add(entree_panel(), "entree");
         card_panel.add(salads_panel(), "salads");
+        card_panel.add(sides_panel(), "sides");
+        card_panel.add(kids_panel(), "kids");
+        card_panel.add(treats_panel(), "treats");
+        card_panel.add(drinks_panel(), "drinks");
+        card_panel.add(sauces_panel(), "sauces");
         
         frame.add(card_panel, BorderLayout.CENTER);
         frame.add(orderPanel(), BorderLayout.AFTER_LINE_ENDS);
@@ -109,11 +124,26 @@ public class Cashier implements ActionListener {
         if (e.getSource() == breakfast) {
             cardLayout.show(card_panel, "breakfast");
         }
-        if (e.getSource() == entree) {
+        else if (e.getSource() == entree) {
             cardLayout.show(card_panel, "entree");
         }
-        if (e.getSource() == salads) {
+        else if (e.getSource() == salads) {
             cardLayout.show(card_panel, "salads");
+        }
+        else if (e.getSource() == sides) {
+            cardLayout.show(card_panel, "sides");
+        }
+        else if (e.getSource() == kids) {
+            cardLayout.show(card_panel, "kids");
+        }
+        else if (e.getSource() == treats) {
+            cardLayout.show(card_panel, "treats");
+        }
+        else if (e.getSource() == drinks) {
+            cardLayout.show(card_panel, "drinks");
+        }
+        else if (e.getSource() == dipping) {
+            cardLayout.show(card_panel, "sauces");
         }
     }
 
@@ -192,6 +222,136 @@ public class Cashier implements ActionListener {
             salads_panel.add(innerPanel);
         }
         return salads_panel;
+
+    }
+
+    private JPanel sides_panel() {
+
+        JPanel sides_panel = new JPanel(new GridLayout(10, 3, 10, 10));
+        for (int i = 0; i < menuItems.get("Sides").size(); i++) {
+            JPanel innerPanel = new JPanel(new BorderLayout());
+			innerPanel.setBackground(Color.WHITE);
+            JLabel name = new JLabel(menuItems.get("Sides").get(i)[0]);
+            JLabel price = new JLabel(menuItems.get("Sides").get(i)[1]);
+            JTextField quantity = new JTextField(10);
+            name.setHorizontalAlignment(JLabel.CENTER);
+			innerPanel.add(name, BorderLayout.BEFORE_FIRST_LINE);
+            // quantity.addActionListener(new ActionListener() {
+            //     public void actionPerformed(ActionEvent e) {
+            //         String input = quantity.getText();
+            //         quantity.setText(input);
+            //     }
+            // });
+            price.setHorizontalAlignment(JLabel.CENTER);
+            innerPanel.add(price);
+            innerPanel.add(quantity, BorderLayout.AFTER_LAST_LINE);
+            sides_panel.add(innerPanel);
+        }
+        return sides_panel;
+
+    }
+
+    private JPanel kids_panel() {
+
+        JPanel kids_panel = new JPanel(new GridLayout(10, 3, 10, 10));
+        for (int i = 0; i < menuItems.get("Kids_Meals").size(); i++) {
+            JPanel innerPanel = new JPanel(new BorderLayout());
+			innerPanel.setBackground(Color.WHITE);
+            JLabel name = new JLabel(menuItems.get("Kids_Meals").get(i)[0]);
+            JLabel price = new JLabel(menuItems.get("Kids_Meals").get(i)[1]);
+            JTextField quantity = new JTextField(10);
+            name.setHorizontalAlignment(JLabel.CENTER);
+			innerPanel.add(name, BorderLayout.BEFORE_FIRST_LINE);
+            // quantity.addActionListener(new ActionListener() {
+            //     public void actionPerformed(ActionEvent e) {
+            //         String input = quantity.getText();
+            //         quantity.setText(input);
+            //     }
+            // });
+            price.setHorizontalAlignment(JLabel.CENTER);
+            innerPanel.add(price);
+            innerPanel.add(quantity, BorderLayout.AFTER_LAST_LINE);
+            kids_panel.add(innerPanel);
+        }
+        return kids_panel;
+
+    }
+
+    private JPanel treats_panel() {
+
+        JPanel treats_panel = new JPanel(new GridLayout(10, 3, 10, 10));
+        for (int i = 0; i < menuItems.get("Treats").size(); i++) {
+            JPanel innerPanel = new JPanel(new BorderLayout());
+			innerPanel.setBackground(Color.WHITE);
+            JLabel name = new JLabel(menuItems.get("Treats").get(i)[0]);
+            JLabel price = new JLabel(menuItems.get("Treats").get(i)[1]);
+            JTextField quantity = new JTextField(10);
+            name.setHorizontalAlignment(JLabel.CENTER);
+			innerPanel.add(name, BorderLayout.BEFORE_FIRST_LINE);
+            // quantity.addActionListener(new ActionListener() {
+            //     public void actionPerformed(ActionEvent e) {
+            //         String input = quantity.getText();
+            //         quantity.setText(input);
+            //     }
+            // });
+            price.setHorizontalAlignment(JLabel.CENTER);
+            innerPanel.add(price);
+            innerPanel.add(quantity, BorderLayout.AFTER_LAST_LINE);
+            treats_panel.add(innerPanel);
+        }
+        return treats_panel;
+
+    }
+
+    private JPanel drinks_panel() {
+
+        JPanel drinks_panel = new JPanel(new GridLayout(10, 3, 10, 10));
+        for (int i = 0; i < menuItems.get("Drinks").size(); i++) {
+            JPanel innerPanel = new JPanel(new BorderLayout());
+			innerPanel.setBackground(Color.WHITE);
+            JLabel name = new JLabel(menuItems.get("Drinks").get(i)[0]);
+            JLabel price = new JLabel(menuItems.get("Drinks").get(i)[1]);
+            JTextField quantity = new JTextField(10);
+            name.setHorizontalAlignment(JLabel.CENTER);
+			innerPanel.add(name, BorderLayout.BEFORE_FIRST_LINE);
+            // quantity.addActionListener(new ActionListener() {
+            //     public void actionPerformed(ActionEvent e) {
+            //         String input = quantity.getText();
+            //         quantity.setText(input);
+            //     }
+            // });
+            price.setHorizontalAlignment(JLabel.CENTER);
+            innerPanel.add(price);
+            innerPanel.add(quantity, BorderLayout.AFTER_LAST_LINE);
+            drinks_panel.add(innerPanel);
+        }
+        return drinks_panel;
+
+    }
+
+    private JPanel sauces_panel() {
+
+        JPanel sauces_panel = new JPanel(new GridLayout(10, 3, 10, 10));
+        for (int i = 0; i < menuItems.get("Sauces").size(); i++) {
+            JPanel innerPanel = new JPanel(new BorderLayout());
+			innerPanel.setBackground(Color.WHITE);
+            JLabel name = new JLabel(menuItems.get("Sauces").get(i)[0]);
+            JLabel price = new JLabel(menuItems.get("Sauces").get(i)[1]);
+            JTextField quantity = new JTextField(10);
+            name.setHorizontalAlignment(JLabel.CENTER);
+			innerPanel.add(name, BorderLayout.BEFORE_FIRST_LINE);
+            // quantity.addActionListener(new ActionListener() {
+            //     public void actionPerformed(ActionEvent e) {
+            //         String input = quantity.getText();
+            //         quantity.setText(input);
+            //     }
+            // });
+            price.setHorizontalAlignment(JLabel.CENTER);
+            innerPanel.add(price);
+            innerPanel.add(quantity, BorderLayout.AFTER_LAST_LINE);
+            sauces_panel.add(innerPanel);
+        }
+        return sauces_panel;
 
     }
 
